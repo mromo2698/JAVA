3c0796fb967f032bcac3be8c836acdd8cea82079
Marcelo Andrés Romo Contreras <46981032+mromo2698@users.noreply.github.com>
2019-04-12 12:47:48 -0400

    Add files via upload

---
+++ Generics & Collections/Ejercicio7.java
@@ -0,0 +1,25 @@
+
+/**
+ * 
+ * @author mromo
+ *
+ * @param <T>
+ */
+public class Ejercicio7<T extends Number> {
+
+	private T numero;
+	
+	public Ejercicio7(T numero) { 
+		this.numero = numero;
+	} 
+	
+	public void dividir() { 
+		System.out.println(numero.doubleValue() / 2); 
+	}
+	
+	public static void main (String[] args) {
+		Ejercicio7<Integer> e = new Ejercicio7<Integer>(14);
+		e.dividir();
+	}
+	
+}
